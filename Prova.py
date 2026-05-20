4120981a818789c523b9022b85593972d6008479
daniele <danielepopones@gmail.com>
2026-05-20 10:29:27 +0200

    creato un app base che chiede all'utente come sta

--- Prova.py
+++ Prova.py
@@ -7,18 +7,26 @@ class Window(QWidget):
         
         self.setWindowTitle("La mia prima vera app")
         self.resize(600, 400)
-        self.label = QLabel("Benvenuto Nella mia app")
-        self.Button = QPushButton("Premi Questo Pulsante")
-        self.Button.clicked.connect(self.change_text)
+        self.label = QLabel("Benvenuto")
+        self.label1 = QLabel("come stai oggi?")
+        self.Button = QPushButton("Sto Bene!")
+        self.Button1 = QPushButton("sto male")
+        self.Button.clicked.connect(self.change_text_bene)
+        self.Button1.clicked.connect(self.change_text_male)
         layout = QVBoxLayout()
         
         layout.addWidget(self.label)
+        layout.addWidget(self.label1)
+        layout.addWidget(self.Button1)
         layout.addWidget(self.Button)
         
         self.setLayout(layout)
         
-    def change_text(self):
-        self.label.setText("Bottone Premuto")
+    def change_text_bene(self):
+        self.label.setText("Sono contento, Spero te passi una buona giornata")
+        
+    def change_text_male(self):
+        self.label.setText("mi dispiace tanto, spero te possa migliorare la tua giornata")
         
 app = QApplication(sys.argv)
 
