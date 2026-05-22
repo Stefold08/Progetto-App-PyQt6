ff06c5fd452986ddeb6b8fbd997dc25a97a68854
daniele <danielepopones@gmail.com>
2026-05-22 10:35:21 +0200

    create altre funzioni

--- Sudoku.py
+++ Sudoku.py
@@ -48,6 +48,52 @@ class Sudoku(QWidget):
         main_layout.addWidget(self.message)
         
         check_btn = QPushButton("Controlla")
-        check_btn = QPushButton("Pulisci")
+        clear_btn = QPushButton("Pulisci")
         
+        check_btn.clicked.connect(self.check_board)
+        clear_btn.clicked.connect(self.clear_board)
+        main_layout.addWidget(check_btn)
+        main_layout.addWidget(clear_btn)
+
+        self.setLayout(main_layout)
+        
+    def validate_input(self, cell):
+        text = cell.text()
+        
+        if text and text not in "123456789":
+            cell.setText("")
+            
+    def get_board(self):
+        board = []
+        
+        for row in self.cells:
+            
+            values = []
+            
+            for cell in row:
                 
+                value = cell.text()
+                
+                values.append(int(value) if value else 0)
+                
+            board.append(values)
+
+        return board
+    
+    def valid_group(self, numbers):
+        
+        values = [n for n in numbers if n != 0]
+        
+        return len(values) == len(set(values))
+    
+    def check_board(self):
+        bouard = self.get_board()
+        
+        for row in board:
+            if not self.valid_group(row):
+                self.message.setText("Errore nelle righe")
+                return
+            
+        for col in range(9):
+            
+            values = []
