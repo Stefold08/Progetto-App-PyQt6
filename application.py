99f8c405e1dc3caca9bc845a2551a010fa7d1c26
daniele <danielepopones@gmail.com>
2026-05-29 08:41:57 +0200

    finito il codice di base da poi aggiornarre in un futuro

--- application.py
+++ application.py
@@ -23,4 +23,35 @@ class MainWindow(QWidget):
         home_page = QWidget()
         home_layout = QVBoxLayout()
         
-        home_layout.addWidget
+        home_layout.addWidget(QLabel("Benvenuto nella Home"))
+        
+        home_page.setLayout(home_layout)
+        
+        #Page settings
+        settings_page = QWidget()
+        settings_layout = QVBoxLayout()
+        
+        settings_layout.addWidget(QLabel("Impostazioni"))
+        
+        settings_page.setLayout(settings_layout)
+        
+        self.pages.addWidget(home_page)
+        self.pages.addWidget(settings_page)
+        
+        #Bottoni Di Navigazione
+        self.home_btn.clicked.connect(lambda: self.pages.setCurrentIndex(0))
+        
+        self.settings_btn.clicked.connect(lambda: self.pages.setCurrentIndex(1))
+        
+        #Layout Finale
+        main_layout.addLayout(sidebar)
+        main_layout.addWidget(self.pages)
+        
+        self.setLayout(main_layout)
+        
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
